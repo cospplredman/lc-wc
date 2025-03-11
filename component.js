072a7cacfe1928a8a43822236819645261d08949
cospplredman <62368514+cospplredman@users.noreply.github.com>
2025-03-10 22:46:04 -0500

    Add files via upload

---
+++ component.js
@@ -0,0 +1,248 @@
+
+/*CPS stuff*/
+let all = (s, ...x) => x.reduce((f, g) => (...x) => f(g(...x)), s);
+let id = (x) => x;
+
+let to_cps = (fn) => (nxt) => (...x) => nxt(fn(...x));
+let un_cps = (fn) => fn(id);
+
+
+
+
+/*CSS stuff*/
+let gen_sheet = document.createElement("style");
+document.head.appendChild(gen_sheet);
+let gen_styles = gen_sheet.sheet;
+
+let gen_rule = (sel, style) => gen_styles.insertRule(`${sel} { ${Object.entries(style).map(v=>v[0].replace(/([A-Z])/g,"-$1") + ":" + v[1]).join(";")}; } \n`);
+let gen_class = (name, style) => gen_rule("." + name, style);
+
+gen_rule("body", {
+	"position": "relative",
+	"width": "100vw",
+	"min-width": "100vw",
+	"height": "100vh",
+	"min-height": "100vh",
+	"overflow": "hidden",
+	"border": "0",
+	"margin": "0",
+	"padding": "0",
+	"touch-action": "none"
+
+});
+
+gen_rule(".Font", {
+	"font-family": `"Nimbus Mono PS", monospace`,
+	"font-size": "12px",
+	"line-height": "14px",
+	"tab-size": "2"
+});
+
+gen_rule(".Font::selection", {
+	"color": "white",
+	"background-color": "black"
+});
+
+gen_rule(".Rect", {
+	"max-width": "100%",
+	"max-height": "100%",
+	"position": "absolute",
+	"white-space": "wrap",
+	"word-break": "break-all",
+	"border": "0",
+	"margin": "0",
+	"padding": "0",
+	"touch-action": "none",
+	"overflow": "hidden",
+	"resize": "none",
+	"outline": "none",
+	"font-variant-ligatures": "none",
+	"scrollbar-width": "thin",
+	"background-color": "white"
+});
+
+
+let el = (type) => to_cps(() => document.createElement(type));
+
+let Children = (...x) => to_cps((el) => {
+	el.replaceChildren(...x.filter(v => v != null).map(v => un_cps(v)()));
+	return el;
+});
+
+let Style = (style) => to_cps((el) => {
+	Object.entries(style).forEach(v=>el.style[v[0]] = v[1])
+	return el;
+});
+
+let Class = (cl) => to_cps((el) => {
+	el.classList.add(cl);
+	return el;
+});
+
+let SetText = (value) => to_cps((el) => {
+	el.textContent = value;
+	return el;
+});
+
+let Attr = (name, value) => to_cps((el) => {
+	el.setAttribute(name, value);
+	return el;
+});
+
+let EventListener = (name, cb) => to_cps((el) => {
+	el.addEventListener(name, cb);
+	return el;
+});
+
+
+
+
+
+
+let Body = (...nd) => all(
+	el("body"),
+	Children(...nd)
+);
+
+let Rect = (x, y, w, h) => (...nd) => all(
+	el("pre"),
+	Class("Rect"),
+	Style({
+		top: `calc(${y})`,
+		left: `calc(${x})`,
+		width: `calc(${w})`,
+		height: `calc(${h})`,
+		"min-width": `calc(${w})`,
+		"min-height": `calc(${h})`
+	}),
+	Children(...nd)
+);
+
+let Border = (t, c = "white") => (...nd) => all(
+	Rect(t, t, `100% - 2*(${t})`, `100% - 2*(${t})`)(
+		...nd
+	),
+	Style({"background-color": c})
+);
+
+let Scroll = (nd) => all(
+	nd,
+	Style({"overflow-y": "scroll"})
+)
+
+let Text = (str) => all(
+	el("pre"),
+	Class("Rect"),
+	Class("Font"),
+	SetText(str)
+);
+
+
+let TextField = (x = "0%", y = "0%", w = "100%", h = "100%") => {
+	let undo_buf = [];
+
+	return all(
+		Rect(x, y, w, h)(),
+		Class("Font"),
+		Style({"word-break": "break-all"}),
+		Attr("spellcheck", "false"),
+		Attr("contenteditable", "plaintext-only"),
+		EventListener('keydown', (e) => {
+			switch (e.keyCode) {
+			case 9: //tab
+				e.preventDefault();
+
+
+				let sel = e.target.ownerDocument.defaultView.getSelection();
+				let range = sel.getRangeAt(0);
+
+				if(range.startOffset == range.endOffset){
+					document.execCommand("insertText", true, "\t");
+				}else{
+					let escapeHTML = (str) => {
+						let div = document.createElement('div');
+						div.textContent = str;
+						return div.innerHTML;
+					}
+
+					let get_char_at = (node, offset) => node.data[offset];
+
+					let get_prev_nl = (range) => {
+						let start = range.startOffset+1;
+						let node = range.startContainer;
+
+						while(1){
+							while(start-- && get_char_at(node, start) != "\n");
+							if(start >= 0)
+								return [node, start];
+							if(!node.previousSibling)
+								return [node, 0];
+							
+							node = node.previousSibling;
+							start = node.length;
+						}
+					};
+
+					let dec_range_start = (range, dist) => {
+						let start = range.startOffset;
+						let node = range.startContainer;
+
+						if(start >= dist){
+							range.setStart(node, start - dist);
+							return;
+						}
+
+						dist -= start;
+
+						while(dist > node.length && node.previousSibling){
+							dist -= node.length;
+							node = node.previousSibling;
+						}
+
+						if(dist >= node.length){
+							range.setStartBefore(node);
+							return;
+						}
+
+
+						range.setStart(node, node.length - dist);
+					}
+
+					range.setStart(...get_prev_nl(range));
+					sel.removeAllRanges();
+					sel.addRange(range);
+
+					let str = sel.toString();
+
+					if(e.shiftKey){
+						if(str[0] == "\t")
+							str = str.slice(1);
+
+						str = str.replaceAll("\n\t", "\n");
+					}else{
+						if(str[0] != "\n")
+							str = "\t" + str;
+						str = str.replaceAll(/\n(?!\n)/g, "\n\t");
+					}
+					document.execCommand("insertHTML", true, escapeHTML(str));
+
+					range = sel.getRangeAt(0);
+					dec_range_start(range, str.length);
+					sel.removeAllRanges();
+					sel.addRange(range);
+
+
+				}
+
+				/*range.deleteContents();
+				range.insertNode(tabNode);
+				range.setStartAfter(tabNode);
+				range.setEndAfter(tabNode); 
+				sel.removeAllRanges();
+				sel.addRange(range);*/
+			break;
+			}
+		})
+	);
+};
+
---
+++ interp.js
@@ -0,0 +1,168 @@
+let lc_print;
+
+let lc_eval = (() => {
+	let stack_machine = (fn) => (nxt) => (x) => {
+		let stack, arg;
+
+		let ret = (x) => arg = x;
+		let call = (fn) => (nxt) => (x) => {
+			if(nxt != ret) stack.push(nxt);
+			stack.push(fn(ret, call));
+			arg = x;
+		}
+
+		stack = [fn(ret, call)];
+		arg = x;
+		while(stack.length) stack.pop()(arg);
+		return nxt(arg);
+	}
+
+	let sm_compose = (f, g) => (ret, call) => call(g)(call(f)(ret));
+	let sm_all = (...arr) => arr.reduce(sm_compose);
+	let to_sm = (fn) => (ret, call) => (x) => ret(fn(x));
+	let un_sm = (fn) => stack_machine(fn)(I);
+
+	let to_cl = ([...l]) => l.toReversed().reduce((a, b) => [b, a], []);                                                  
+	let I = x => x;                                                      
+	let C = x => y => z => x(z)(y);
+	let log = (f) => (x) => (console.log(x), f(x));
+
+	let PushStr = v => c => (ret, call) => ([str, stack]) => call(c)(ret)([[v, str], stack]);
+	let PopStr = f => c => (ret, call) => ([str, stack]) => str.length 
+		? call(f(str[0])(c))(ret)([str[1], stack]) 
+		: ret(null);
+
+	let PushStack = v => c => (ret, call) => ([str, stack]) => call(c)(ret)([str, [v, stack]]);
+	let PopStack = f => c => (ret, call) => ([str, stack]) => stack.length 
+		? call(f(stack[0])(c))(ret)([str, stack[1]]) 
+		: ret(null);
+
+	let Fail = (ret, call) => ([str, stack]) => ret(null);
+	let Either = a => b => c => (ret, call) => ([str, stack]) => call(a(c))(x => x == null ? call(b(c))(ret)([str, stack]) : ret(x))([str, stack]);
+
+	let all = (...f) => f.reduce((f, g) => x => f(g(x)));
+	let some = (...f) => f.reduce((f, g) => Either(f)(g));
+
+	let Del = PopStack (a => I);
+	let End = (ret, call) => ([str, stack]) => str.length == 0 ? ret([str, stack]) : ret(null);
+	let Pred = f => PopStr(v => c => f(v) ? PushStack(v)(c) : Fail);
+	let In = l => Pred(v => l.some(k => k == v));
+	let Rx = r => Pred(v => r.test(v) && v != null);
+	let Opt = C(Either)(I);
+	let Eq = all(Pred, a => b => a == b);
+
+	let List = f => {
+		let ret = (x) => ret(x);
+		return ret = all(f, some(
+			all(ret, PopStack(a => PopStack(b => PushStack([b, ...a])))), 
+			PopStack(x => PushStack([x]))));
+	}
+
+	let Thunk = uneval => ({type: "thunk", uneval, eval: null});
+	let Lit = value => ({type: "lit", value});
+
+	let Sym = name => e => e[name] == null ? Lit(name) : e[name];
+	let App = (f, p) => e => ({type: "app", func: f(e), param: p(e)});
+	let Func = (p, b) => e => ({type: "func", func: (x) => {
+		let n_e = {...e}; n_e[p] = x;
+		return b(n_e);
+	}});
+
+	let MultiFunc = (p, b) => p.reduceRight((b, a) => Func(a, b), b); 
+
+	let lc_grammar = (c) => {
+		let wspace = Opt(all(List(Rx(/\s/)), Del));
+		let Paren = f => all(wspace, Eq("("), Del, f, wspace, Eq(")"), Del);
+		let char = Rx(/[^\s\(\)\.\\λ]/);
+		let lambda = some(Eq("λ"), Eq("\\"));
+		let identifier = all(wspace, List(char));
+
+		let expr = (x) => expr(x);
+
+		let term = some(Paren(expr), all(identifier, PopStack(x => PushStack(Sym(x.join(""))))));
+		let app = all(List(term), PopStack(x => PushStack(x.reduce(App))));
+		let func = all(
+			wspace, lambda, Del, 
+			List(identifier), wspace,
+			Eq("."), Del,
+			expr, PopStack(b => PopStack(p => PushStack(MultiFunc(p.map(v => v.join("")), b)))));
+
+		expr = some(all(app, Opt(all(func, PopStack(b => PopStack(a => PushStack(App(a, b))))))), func);
+		return all(expr, wspace)(c);
+	}
+
+	let lc_normal = (ret, call) => t => ({
+		"lit": ({value}) => ret(value),
+		"thunk": ({eval}) => eval == null ? call(lc_normal)(val => ret(t.eval = val))(t.uneval) : ret(eval),
+		"app": ({func, param}) => call(lc_normal)(fn => call(fn)(ret)(Thunk(param)))(func),
+		"func": ({func}) => ret((ret, call) => x => call(lc_normal)(ret)(func(x)))
+	}[t.type])(t);
+
+
+
+
+
+
+	let gather_args = (n) => (nxt) => {
+		let me = (args) => (ret, call) => (a) => {
+			let n_args = [...args, a];
+
+			if(n_args.length < n){
+				ret(me(n_args));
+			}else{
+				call(nxt)(ret)(n_args);
+			}
+		}
+		return me([]);
+	}
+		
+	let b = (n) => (fn) => Lit(gather_args(n)((ret, call) => (args) => {
+		ret(fn(...args.map(un_sm(lc_normal))))
+	}));
+
+	let True = (ret, call) => (a) => ret((ret, call) => (b) => call(lc_normal)(ret)(a));
+	let False = (ret, call) => (a) => ret((ret, call) => (b) => call(lc_normal)(ret)(b));
+	let Bool = (v) => v ? True : False;
+	let env = {
+		//Strictly first arg and binds it to second
+		";": Lit((ret, call) => x => ret(
+			(ret, call) => f => call(lc_normal)(
+				(val) => call(lc_normal)(ret)({type: "app", func: f, param: Lit(val)})
+			)(x)
+		)),
+
+		print: b(1)(x => (lc_print(x), x)),
+		num: b(1)(BigInt),
+		char: b(1)((n) => String.fromCharCode(Number(n))),
+		"+": b(2)((a, b) => a + b),
+		"-": b(2)((a, b) => a - b),
+		"*": b(2)((a, b) => a * b),
+		"/": b(2)((a, b) => a / b),
+		"^": b(2)((a, b) => a ^ b),
+		"<<": b(2)((a, b) => a << b),
+		">>": b(2)((a, b) => a >> b),
+		"&": b(2)((a, b) => a & b),
+		"|": b(2)((a, b) => a | b),
+		"~": b(1)((a) => ~a),
+		"<": b(2)((a, b) => Bool(a < b)),
+		"<=": b(2)((a, b) => Bool(a <= b)),
+		">": b(2)((a, b) => Bool(a > b)),
+		">=": b(2)((a, b) => Bool(a >= b)),
+		"==": b(2)((a, b) => Bool(a == b)),
+		"!=": b(2)((a, b) => Bool(a != b))
+	};
+
+
+	let lc_eval = (str) => {
+		let state = un_sm(lc_grammar(End)) ([to_cl(str), []]);
+
+		if(state){
+			let tr = state[1][0](env);
+			return un_sm(lc_normal)(tr)
+		}
+
+		return null;
+	}
+
+	return lc_eval;
+})();
